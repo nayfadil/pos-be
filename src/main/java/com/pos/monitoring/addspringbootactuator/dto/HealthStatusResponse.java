306fd15e8fad46d792a09818efd134a7aee16e17
nayfadil <42664410+nayfadil@users.noreply.github.com>
2026-08-14 16:22:12 +0800

    feat(be): add-spring-boot-actuator

--- src/main/java/com/pos/monitoring/addspringbootactuator/dto/HealthStatusResponse.java
+++ src/main/java/com/pos/monitoring/addspringbootactuator/dto/HealthStatusResponse.java
@@ -1,7 +1,12 @@
 package com.pos.monitoring.addspringbootactuator.dto;
 
+import java.time.LocalDateTime;
+
 public record HealthStatusResponse(
-        String status,
-        String message,
-        long timestamp
+    String status,
+    String databaseStatus,
+    long freeMemory,
+    long totalMemory,
+    long freeDiskSpace,
+    LocalDateTime timestamp
 ) {}
