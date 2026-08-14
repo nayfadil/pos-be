d0ef5e2d08155cea1eb3b29cdc9c84c9311b9362
nayfadil <42664410+nayfadil@users.noreply.github.com>
2026-08-14 17:31:06 +0800

    feat(be): add-swagger-openapi-docs

---
+++ src/main/java/com/pos/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.pos.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("POS API Documentation")
+                        .version("1.0")
+                        .description("Dokumentasi API untuk aplikasi Point of Sales (POS)."));
+    }
+}
