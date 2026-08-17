cbb909717870a5ac9c5acd8962648f32094b45b4
nayfadil <42664410+nayfadil@users.noreply.github.com>
2026-08-17 16:14:12 +0800

    feat(be): fix-auth-login-flow-compilation-errors

--- src/main/java/com/pos/auth/authloginflow/service/AuthLoginFlowService.java
+++ src/main/java/com/pos/auth/authloginflow/service/AuthLoginFlowService.java
@@ -5,6 +5,8 @@ import com.pos.auth.authloginflow.dto.LoginResponse;
 import com.pos.auth.rsakey.dto.PublicKeyResponse;
 
 public interface AuthLoginFlowService {
-    PublicKeyResponse getPublicKey();
+
     LoginResponse login(LoginRequest request);
+
+    PublicKeyResponse getPublicKey();
 }
