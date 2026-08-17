d205172a7c1da4cca30f401fcad7c05b4fca4a77
nayfadil <42664410+nayfadil@users.noreply.github.com>
2026-08-17 16:02:30 +0800

    feat(be): fix-rsa-decrypt-static-compilation-error

--- src/main/java/com/pos/auth/rsakey/util/RsaEncryptionUtil.java
+++ src/main/java/com/pos/auth/rsakey/util/RsaEncryptionUtil.java
@@ -1,27 +1,38 @@
 package com.pos.auth.rsakey.util;
 
-import com.pos.auth.rsakey.exception.InvalidRsaKeyException;
-import org.springframework.stereotype.Component;
-
 import javax.crypto.Cipher;
 import java.nio.charset.StandardCharsets;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.Base64;
 
-@Component
 public class RsaEncryptionUtil {
 
-    private static final String ALGORITHM = "RSA/ECB/PKCS1Padding";
+    private static final String RSA_ALGORITHM = "RSA";
 
-    public String decrypt(String encryptedTextBase64, PrivateKey privateKey) {
+    private RsaEncryptionUtil() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static String encrypt(String plainText, PublicKey publicKey) {
         try {
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            byte[] encryptedBytes = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Error encrypting data with RSA key", e);
+        }
+    }
+
+    public static String decrypt(String encryptedText, PrivateKey privateKey) {
+        try {
+            Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
             cipher.init(Cipher.DECRYPT_MODE, privateKey);
-            byte[] bytes = Base64.getDecoder().decode(encryptedTextBase64);
-            byte[] decryptedBytes = cipher.doFinal(bytes);
+            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
             return new String(decryptedBytes, StandardCharsets.UTF_8);
         } catch (Exception e) {
-            throw new InvalidRsaKeyException("Failed to decrypt data with provided RSA key", e);
+            throw new RuntimeException("Error decrypting data with RSA key", e);
         }
     }
 }
